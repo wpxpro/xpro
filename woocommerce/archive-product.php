2cd869f4a499cc97ba28e87494d856c38daaf1eb
Farman Akram <farman.topnotch@gmail.com>
2021-06-30 12:34:42 +0500

    30/6/21

--- woocommerce/archive-product.php
+++ woocommerce/archive-product.php
@@ -1,58 +1,83 @@
 <?php
 /**
- * The shop archive template file
- * @package Xpro
+ * The Template for displaying product archives, including the main shop page which is a post type archive
+ *
+ * This template can be overridden by copying it to yourtheme/woocommerce/archive-product.php.
+ *
+ * HOWEVER, on occasion WooCommerce will need to update template files and you
+ * (the theme developer) will need to copy the new files to your theme to
+ * maintain compatibility. We try to do this as little as possible, but it does
+ * happen. When this occurs the version of the template file will be bumped and
+ * the readme will list any important changes.
+ *
+ * @see https://docs.woocommerce.com/document/template-structure/
+ * @package WooCommerce\Templates
+ * @version 3.4.0
  */
 
-// Exit if accessed directly.
 defined( 'ABSPATH' ) || exit;
 
 get_header();
 
-$post_layout = xpro_get_option('xpro_archive_post_layout');
-$layout = xpro_get_option( 'xpro_archive_layout','right-layout');
-$post_layout = xpro_get_option('xpro_archive_post_layout','classic');
+
+$layout = xpro_get_option( 'xpro_shop_layout','right-layout');
 
 ?>
 
-	<main class="xpro-main xpro-post-<?php echo esc_attr($post_layout); ?>">
+<main class="xpro-main xpro-post-classic">
 
-		<?php
+<?php
 
-		if ( have_posts() ) {
+if ( woocommerce_product_loop() ) {
 
-			/* Check shop page */
-			wc_get_template_part( 'content', 'before-category' );
+	/**
+	 * Hook: woocommerce_before_shop_loop.
+	 *
+	 * @hooked woocommerce_output_all_notices - 10
+	 * @hooked woocommerce_result_count - 20
+	 * @hooked woocommerce_catalog_ordering - 30
+	 */
+	do_action( 'woocommerce_before_shop_loop' );
 
-			woocommerce_product_xpro_loop_start();
+	woocommerce_product_loop_start();
 
-			// Start the Loop.
-			while ( have_posts() ) {
+	if ( wc_get_loop_prop( 'total' ) ) {
+		while ( have_posts() ) {
+			the_post();
 
-				the_post();
+			/**
+			 * Hook: woocommerce_shop_loop.
+			 */
+			do_action( 'woocommerce_shop_loop' );
 
-				wc_get_template_part( 'content', 'product' );
-
-			}
-
-			woocommerce_product_xpro_loop_end();
-
-			/* Check shop page */
-			wc_get_template_part( 'content', 'after-category' );
-
-
-			xpro_pagination();
-
-		} else {
-			get_template_part( 'template-parts/content', 'none' );
+			wc_get_template_part( 'content', 'product' );
 		}
 
-		?>
+	}
 
-	</main>
+	woocommerce_product_loop_end();
+
+	/**
+	 * Hook: woocommerce_after_shop_loop.
+	 *
+	 * @hooked woocommerce_pagination - 10
+	 */
+	do_action( 'woocommerce_after_shop_loop' );
+} else {
+	/**
+	 * Hook: woocommerce_no_products_found.
+	 *
+	 * @hooked wc_no_products_found - 10
+	 */
+	get_template_part( 'template-parts/content', 'none' );
+}
+
+?>
+
+</main>
 
 <?php
 
 if($layout != 'full-layout' ): do_action('xpro_woo_sidebar'); endif;
 
-get_footer();
+get_footer( 'shop' );
--- woocommerce/single-product.php
+++ woocommerce/single-product.php
@@ -1,50 +1,38 @@
 <?php
 /**
- * The single shop template file
- * @package Xpro
+ * The Template for displaying all single products
+ *
+ * This template can be overridden by copying it to yourtheme/woocommerce/single-product.php.
+ *
+ * HOWEVER, on occasion WooCommerce will need to update template files and you
+ * (the theme developer) will need to copy the new files to your theme to
+ * maintain compatibility. We try to do this as little as possible, but it does
+ * happen. When this occurs the version of the template file will be bumped and
+ * the readme will list any important changes.
+ *
+ * @see         https://docs.woocommerce.com/document/template-structure/
+ * @package     WooCommerce\Templates
+ * @version     1.6.4
  */
 
-// Exit if accessed directly.
-defined( 'ABSPATH' ) || exit;
+if ( ! defined( 'ABSPATH' ) ) {
+	exit; // Exit if accessed directly
+}
 
-get_header();
+get_header( 'shop' ); ?>
 
-?>
+<main class="xpro-main xpro-shop-single">
 
-	<main class="xpro-main xpro-shop-single">
+<?php while ( have_posts() ) : ?>
+	<?php the_post(); ?>
 
-		<?php
+	<?php wc_get_template_part( 'content', 'single-product' ); ?>
 
-		if ( have_posts() ) {
+<?php endwhile; // end of the loop. ?>
 
-
-			/* Check shop page */
-			wc_get_template_part( 'content', 'before-product' );
-
-			// Start the Loop.
-			while ( have_posts() ) {
-
-				the_post();
-
-				wc_get_template_part( 'content', 'single-product' );
-
-			}
-
-			/* Check shop page */
-			wc_get_template_part( 'content', 'after-product' );
-
-
-			xpro_pagination();
-
-		} else {
-			get_template_part( 'template-parts/content', 'none' );
-		}
-
-		?>
-
-	</main>
+</main>
 
 <?php
+get_footer( 'shop' );
 
-
-get_footer();
+/* Omit closing PHP tag at the end of PHP files to avoid "headers already sent" issues. */
