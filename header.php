ff0199e4f486b7249583915fe90c8ee14ec09eff
Farman Akram <farman.topnotch@gmail.com>
2021-08-24 13:43:59 +0500

    24/8/21

--- header.php
+++ header.php
@@ -11,11 +11,11 @@
 defined( 'ABSPATH' ) || exit;
 
 ?>
-<!DOCTYPE html>
+<!doctype html>
 <html <?php language_attributes(); ?>>
 <head>
 	<meta charset="<?php bloginfo( 'charset' ); ?>">
-	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<link rel="profile" href="http://gmpg.org/xfn/11">
 	<?php wp_head(); ?>
 </head>
